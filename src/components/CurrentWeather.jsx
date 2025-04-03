335126f06df3a82549ade676823dc27964eb4d42
Madalina294 <puicanmadalinamaria@gmail.com>
2025-04-04 02:23:37 +0300

    CurrentWeather added

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -5,14 +5,14 @@ function CurrentWeather({ weather }) {
     return (
         <div className="weather-panel today">
             <h2 className='section-title'>Weather in {weather.city} today</h2>
-            <div className="weather-panel">
-                <img src={weather.icon} alt="weather-icon" />
-                <div className="weather-details">
+            <div className="weather-details">
+                <img src={`icons/${weather.icon}.svg`} alt="weather-icon" />
+                
                     <p className="date">{weather.date}</p>
-                    <p>{weather.temperature}</p>
-                    <p>{weather.wind}</p>
-                    <p>{weather.humidity}</p>
-                </div>
+                    <p>Temperature: {weather.temperature}</p>
+                    <p>Wind: {weather.wind} kph</p>
+                    <p>Humidity: {weather.humidity}%</p>
+                
             </div>
            
         </div>
--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import './style.css';
 
-function SearchSection() {
+const SearchSection = ({ getWeatherDetails, searchInputRef }) => {
+    const API_KEY = "b9669f0236a84264a12214827250304";
+  // Handles city search form submission
+  const handleCitySearch = (e) => {
+    e.preventDefault();
+    const input = e.target.querySelector(".search-input");
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${input.value}&days=7`;
+    getWeatherDetails(API_URL); // Fetches weather details for the entered city
+  };
+
     return (
         <div className="search-section">
             <div className="sidebar">
-                <form id="cityForm" className="city-form">
-                    <label for="city">Enter the city</label>
+                <form id="cityForm" className="city-form" action="#"  onSubmit={handleCitySearch}>
                     <input
                         id="city"
-                        type="text"
-                        placeholder="Cugir, Timisoara..."
+                        className='search-input'
+                        type="search"
+                        placeholder="Enter a city..."
+                        ref={searchInputRef}
                         required
                     />
                     <button id="searchBtn" className="btn btnSeach">Search</button>
